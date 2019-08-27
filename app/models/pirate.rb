a33222a11a45e793ff7e6416775d395e73efe62e
Joshua Hill <josh@jlhworkshop.com>
2019-08-27 22:07:09 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,10 +1,17 @@
+    
 class Pirate
-  # has_many :ships
-  @@all = []
-  
-  attr_accessor :name, :weight, :height
-  
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
   def self.all
-    return @@all    
+    PIRATES
   end
 end

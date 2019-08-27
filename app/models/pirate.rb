21cbc084e2c48d755e44ca0f2708e41f4ffbb4c8
Joshua Hill <josh@jlhworkshop.com>
2019-08-27 22:05:43 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  # has_many :ships
+  @@all = []
+  
+  attr_accessor :name, :weight, :height
+  
+  def self.all
+    return @@all    
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  # belongs_to :pirate
+  attr_accessor :name, :type, :booty
+  
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
+  
+  def self.all
+    return Ship.all
+  end
+  
+  def self.clear
+    Ship.destroy_all()
+  end
+  
 end
